edc2647b5aafdfe1547a5bd498cac0e1e574d036
Odette Bahome <odettebahome@gmail.com>
2020-12-08 12:46:02 +0000

    URL shortning I

--- express_server.js
+++ express_server.js
@@ -1,8 +1,16 @@
 const express = require("express");
 const app = express();
 const PORT = 8080; //default port 8080
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.set("view engine", "ejs");
+
+function generateRandomString(max) {
+  max = 6;
+  let randonString =  Math.floor(Math.random() * Math.floor(max));
+  return randonString.join(",");
+}
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
@@ -30,7 +38,16 @@ app.get("/urls", (req, res) => {
   res.render("urls_index", templateVars);
 });
 
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
 app.get("/urls/:shortURL", (req, res) => {
   const templateVars = {shortURL: req.params.shortURL, longURL:req.params.longURL};
   res.render("urls_show", templateVars);
 });
+
+app.post("urls", (req, res) => {
+  console.log(req.body);
+  res.send("Ok");
+})
