4ef46f969c9061405d3e92b29961d2e5e7a75f2b
Odette Bahome <odettebahome@gmail.com>
2020-12-08 03:04:31 +0000

    Render info for single a URL

--- express_server.js
+++ express_server.js
@@ -9,11 +9,6 @@ const urlDatabase = {
   "9sm5xK": "http://www.google.com"
 };
 
-app.get("/urls", (req, res) => {
-  const templateVars = {urls: urlDatabase};
-  res.render("urls_index", templateVars);
-});
-
 app.get("/", (req, res) => {
   res.send("Hello!");
 });
@@ -28,4 +23,14 @@ app.get("/urls.json", (req, res) => {
 
 app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
+});
+
+app.get("/urls", (req, res) => {
+  const templateVars = {urls: urlDatabase};
+  res.render("urls_index", templateVars);
+});
+
+app.get("/urls/:shortURL", (req, res) => {
+  const templateVars = {shortURL: req.params.shortURL, longURL:req.params.longURL};
+  res.render("urls_show", templateVars);
 });
